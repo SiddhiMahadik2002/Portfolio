bc443aee25990c263fca93ae8145b850cd94ab67
Siddhi <siddhimahadik257@gmail.com>
2023-09-12 01:10:55 +0530

    Feat : create home page

---
+++ src/component/portfolio.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "../component/portfolio.css"
+const Portfolio = () => {
+
+
+    return (
+        <div className="main">
+            <div className="left">
+                <div className="info">
+                    <img src="https://media.licdn.com/dms/image/C5603AQEVOY_esXCjDQ/profile-displayphoto-shrink_800_800/0/1660486102148?e=2147483647&v=beta&t=b10MeZxPFVu2olAiB8ZVyC83a3ESUnO4-xZdBbCYV6E" alt="" />
+                    <p>Siddhi Mahadik</p>
+                </div>
+                <div className="menu">
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Projects</p>
+                    <p>GitHub</p>
+                    <p>Contact</p>
+                </div>
+                
+                    <button>Get my Resume</button>
+                
+            </div>
+            <div className="right">
+                <div className="leftside">
+                    <h1>Hi, I'm <span>Siddhi!</span> <br />
+                        Creative Developer <br />from India
+                    </h1>
+                    <p>I'm a India based UIUX designer & front‑end <br />developer with strong knowledge in Javascript ecosystem!
+                    </p>
+                    <div className="btns">
+                        <button>Got a project?</button>
+                        <button>Let's talk</button>
+                    </div>
+                    <div className="creds">
+                        <div class="vertical-line"></div>
+                        <ul>
+                            <li><a href="">8956681803</a>
+                            </li>
+                            <li><a href="">siddhimahadik257@gmail.com</a></li>
+                            <li><a class="href_location" href="#">Chiplun, Maharashtra</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className="rightside">
+                <img src="https://media.licdn.com/dms/image/C5603AQEVOY_esXCjDQ/profile-displayphoto-shrink_800_800/0/1660486102148?e=2147483647&v=beta&t=b10MeZxPFVu2olAiB8ZVyC83a3ESUnO4-xZdBbCYV6E" alt="" />
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+export default Portfolio;
